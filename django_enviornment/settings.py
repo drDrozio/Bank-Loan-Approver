221e9b013cedb4c826dbc279f3cec1ee8c600c2e
drDrozio <ishanss2018@gmail.com>
2021-10-15 13:05:39 +0530

    heroku release

--- django_enviornment/settings.py
+++ django_enviornment/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 #Our models folder 
 MODELS = os.path.join(BASE_DIR, 'main/model_tools')
